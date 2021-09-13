cc38beffec8b6c4e88c0afc5067323fe9001b774
aleksandr-kovbasa <turbo2011d@gmail.com>
2021-09-13 14:10:21 +0300

    start

---
+++ js/script.js
@@ -0,0 +1,25 @@
+// preloader
+$(function() {
+  setTimeout(function() {
+    $("div").removeClass("blur");
+    $("body").addClass("loaded");
+  }, 3000);
+})
+
+// burger menu
+$('.mobile div').on('click', function() {
+  $('.mobile div').toggleClass('active');
+  $('.mobile nav').toggleClass('open');
+  $('.mobile nav ul').toggleClass('show');
+});
+
+
+document.addEventListener('DOMContentLoaded', function () {
+  // инициализация слайдера
+  var slider = new SimpleAdaptiveSlider('.slider', {
+    loop: true,
+    autoplay: true,
+    interval: 5000,
+    swipe: true,
+  });
+});
